b1b2783a9dfdedced1761e494719c6dd10e19154
Györfi Marcell <gyorfimarcell@gmail.com>
2023-06-08 20:45:30 +0200

    Kész

--- web/tesztauzem.js
+++ web/tesztauzem.js
@@ -1,3 +1,21 @@
 ﻿fetch("/api/akcio").then(valasz => valasz.json().then(adatok => {
-    document.getElementById("")
+    document.getElementById("akcios-teszta").innerHTML = adatok["akciosTeszta"];
+    document.getElementById("akcios-ar").innerHTML = adatok["akciosAr"];
 }));
+
+function bekuld() {
+    fetch("/api/jatek", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            uzenet: document.getElementById("jatek-valasz").value
+        })
+    }).then(valasz => {
+        if (valasz.status == 200) {
+            document.getElementById("jatek-valasz").value = "";
+            alert("Köszönjük, hogy részt vesz a játékunkban!");
+        }
+    });
+}
